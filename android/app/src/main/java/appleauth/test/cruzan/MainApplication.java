8888669674a244b717cd17dc91c015049f1cb5ff
cruzach <charliecruzan@gmail.com>
2021-06-11 09:45:42 -0400

    prebuild

--- android/app/src/main/java/appleauth/test/cruzan/MainApplication.java
+++ android/app/src/main/java/appleauth/test/cruzan/MainApplication.java
@@ -1,4 +1,4 @@
-package com.eascli452;
+package appleauth.test.cruzan;
 
 import android.app.Application;
 import android.content.Context;
@@ -11,7 +11,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.eascli452.generated.BasePackageList;
+import appleauth.test.cruzan.generated.BasePackageList;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -110,7 +110,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.eascli452.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("appleauth.test.cruzan.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
